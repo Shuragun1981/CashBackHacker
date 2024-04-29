dd495eb00808456a06657f77b4c1b0fd54a664c9
Aleksandr Volik <Aleksandr_volik@mail.ru>
2024-04-29 14:49:31 +0300

    Add cashbackHacService

--- src/test/java/ru/netology/service/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -1,8 +1,11 @@
 package ru.netology.service;
 
-import org.testng.annotations.Test;
+import org.junit.Test;
 
-import static org.testng.Assert.*;
+import static org.junit.Assert.*;
+//import org.testng.annotations.Test;
+
+//import static org.testng.Assert.*;
 
 public class CashbackHackServiceTest {
 
